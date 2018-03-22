0576b7d6d4a669a56230abc072030513ccd77979
nonamenerd <nonamenerd@gmail.com>
2018-03-22 14:50:32 +0500

    Explicitly define VK api version in auth request (#188)
    VK api vesion parameter must be set now in auth request
    so v=3.0 hardcoded as a quickfix solution

--- hybridauth/Hybrid/Providers/Vkontakte.php
+++ hybridauth/Hybrid/Providers/Vkontakte.php
@@ -110,6 +110,7 @@ class Hybrid_Providers_Vkontakte extends Hybrid_Provider_Model_OAuth2
 		// Vkontakte requires user id, not just token for api access
 		$params['uid'] = Hybrid_Auth::storage()->get( "hauth_session.{$this->providerId}.user_id" );
 		$params['fields'] = implode(',', $this->fields);
+		$params['v'] = '3.0';
 		// ask vkontakte api for user infos
 
 		$response = $this->api->api( 'getProfiles' , 'GET', $params);
